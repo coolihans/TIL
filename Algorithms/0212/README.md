f0e913df66cecc1c3ea980460336bd890930062d
coolihans <hyeunmo.an@gmail.com>
2022-04-04 23:30:04 +0900

    220404 til

--- Algorithms/0212/README.md
+++
@@ -1,9 +0,0 @@
-## swea 제출 적응 
-
-## input 구글링 해보기
-
-## swea 사용해 보기
-1. difficulty 조절 
-2. 댓글 힌트?
-3. runtime error 찾아 보기
-4. 다른 사이트? 백준 등등 들어가 보기..
--- Algorithms/0316/tree2.py
+++
@@ -1,8 +0,0 @@
-# 완전이진트리에서의 순회
-
-def pre_order(v):
-    global last
-    if v <= last:   # 마지막 정점번호 아래면 존재하는 것
-        print(v)        # visit(v)
-        pre_order(v*2)     # 왼쪽 자식정점 방문
-        pre_order(v*2+1)     # 오른쪽 자식정점 방문
