761a5940e028c4605851743e6a47106a955318cb
coolihans <hyeunmo.an@gmail.com>
2022-05-16 23:27:33 +0900

    220516 drf_back

---
+++ Vue/0516-django_back/articles/serializers/comment.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from ..models import Comment
+
+User = get_user_model()
+
+# CUD => validation
+# R => Data serializing
+class CommentSerializer(serializers.ModelSerializer):
+    """
+    write
+    - content
+    read
+    - pk user article
+    """
+    
+    class UserSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = User
+            fields = ('pk', 'username')
+
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ('pk', 'user', 'content', 'article',)
+        read_only_fields = ('article', )
